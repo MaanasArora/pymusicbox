6c1fc07a125daafb0828fbcb29ed5cfb0dd4737f
MaanasArora <maanasarora23@gmail.com>
2022-04-09 03:24:35 -0400

    signal indexing edge case fixes

--- src/pymusicbox/audio/audio_utils.py
+++ src/pymusicbox/audio/audio_utils.py
@@ -1,5 +1,13 @@
+import numpy
+
+
 def add_audio(t, data, data_to_add, sample_rate):
     t_start = int(t * sample_rate)
     t_end = t_start + len(data_to_add)
 
-    data[t_start:t_end] += data_to_add
+    if t_end + 1 > len(data):
+        t_end = len(data) - 1
+
+        data[t_start:t_end] = data_to_add[:t_end-t_start]
+    else:
+        data[t_start:t_end] += data_to_add
