7cb024a9f0c36459e3b318a5d03dd243e55a61fb
MaanasArora <maanasarora23@gmail.com>
2022-11-29 03:11:49 -0500

    harmonic instrument subclass

--- src/pymusicbox/synth/instrument.py
+++ src/pymusicbox/synth/instrument.py
@@ -7,6 +7,30 @@ from pymusicbox.symbolic.symbols import Note, NoteEvent, Track
 
 
 @dataclass
+class Instrument:
+    sample_rate: int = 44100
+    max_amp: float = 1e-5
+
+    def render_note(self, note: Note):
+        note_length = note.length*self.sample_rate
+
+        freq = 55 * pow(2, (note.note / 12))
+        t = np.linspace(0, note.length, int(note_length))
+
+        data = self.max_amp * note.level * np.sin(2. * np.pi * freq * t)
+        return Audio(data, self.sample_rate)
+
+    def render_track(self, track: Track):
+        audio = Audio.empty(track.length, self.sample_rate)
+
+        for event in track:
+            if isinstance(event, NoteEvent):
+                audio.add(event.time, self.render_note(event.note))
+
+        return audio
+
+
+@dataclass
 class HarmonicsConfiguration:
     attack: float
     decay: float
@@ -21,10 +45,8 @@ class HarmonicsConfiguration:
 
 
 @dataclass
-class Instrument:
-    sample_rate: int = 44100
+class HarmonicInstrument(Instrument):
     harmonics: HarmonicsConfiguration = None
-    max_amp: float = 1e-5
 
     def get_harmonic_amps(self, length: float):
         attack, decay, release, sustain_factor = self.harmonics.apply_rate(self.sample_rate)
@@ -41,22 +63,7 @@ class Instrument:
         return np.concatenate([attack_amp, decay_amp, sustain_amp, release_amp])
 
     def render_note(self, note: Note):
-        note_length = note.length*self.sample_rate
+        audio = super().render_note(note)
 
-        freq = 55 * pow(2, (note.note / 12))
-        t = np.linspace(0, note.length, int(note_length))
-
-        data = self.max_amp * note.level * np.sin(2. * np.pi * freq * t)
-        if self.harmonics is not None:
-            data *= self.get_harmonic_amps(note_length)
-
-        return Audio(data, self.sample_rate)
-
-    def render_track(self, track: Track):
-        audio = Audio.empty(track.length, self.sample_rate)
-
-        for event in track:
-            if isinstance(event, NoteEvent):
-                audio.add(event.time, self.render_note(event.note))
-
+        audio.waveform *= self.get_harmonic_amps(note.length * self.sample_rate)
         return audio
